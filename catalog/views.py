5f84d1e6820439cb6677fb3a08adcad7cd7ef6c9
ettoredswlf-hash <ettoredswlf@gmail.com>
2026-01-07 16:36:06 +0100

    feat: add catalog templates for product pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(available=True).order_by("name")
+    return render(request, "catalog/product_list.html", {"products": products})
+
+
+def product_detail(request, pk: int):
+    product = get_object_or_404(Product, pk=pk, available=True)
+    return render(request, "catalog/product_detail.html", {"product": product})
